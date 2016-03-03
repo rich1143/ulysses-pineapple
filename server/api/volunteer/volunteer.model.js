4aa6246d28468f51f1df0f61f99d0c1ed27482a5
devshawn <musicalshawn@gmail.com>
2016-03-03 12:32:10 -0600

    Updating volunteer model

--- server/api/volunteer/volunteer.model.js
+++ server/api/volunteer/volunteer.model.js
@@ -3,9 +3,50 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var VolunteerSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  firstName: String,
+  lastName: String,
+  assoc: String,
+  street1: String,
+  street2: String,
+  city: String,
+  state: String,
+  zip: String,
+  country: String,
+  region: String,
+  phone: String,
+  workPhone, String,
+  email: String,
+  fax: String,
+  assocExp: Number,
+  coachExp: Number,
+  memberExp: Number,
+  username: String,
+  password: String,
+  current: String,
+  jobPreference1: String,
+  jobPreference2: String,
+  membershipNumber: Number,
+  problem: Number,
+  division: Number,
+  submitDate: String,
+  lastModified: String,
+  mName: String,
+  mRegion: String,
+  childTeam: String,
+  coachEmail: String,
+  coachName: String,
+  tshirtSize: String,
+  positionHeld: String,
+  comment: String,
+  certP1: String,
+  certP2: String,
+  certP3: String,
+  certP4: String,
+  certP5: String,
+  certScore: String,
+  certSpont: String,
+  isJudge: Boolean
+
 });
 
 export default mongoose.model('Volunteer', VolunteerSchema);
