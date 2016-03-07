e7119d7f807a2c51ed722e7ce2b63ef1a8ec2636
devshawn <musicalshawn@gmail.com>
2016-03-07 13:42:12 -0600

    Adding create a job functionality

--- client/app/job/job.controller.js
+++ client/app/job/job.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('ulyssesApp')
-  .controller('JobCtrl', function ($scope, Job) {
-    $scope.message = 'Hello';
+  .controller('JobCtrl', function ($scope, $state, Job) {
+    var self = this;
+
+
+    if($state.current.name == "job") {
+
+
+
+    } else if($state.current.name == "job-create") {
+
+      self.createJob = function() {
+        console.log("Clicked submit!");
+        console.log(self.jobtitle);
+        var data = { title: self.jobtitle, description: self.description };
+        Job.save(data);
+        self.jobtitle = "";
+        self.description = "";
+      }
+
+    }
   });
