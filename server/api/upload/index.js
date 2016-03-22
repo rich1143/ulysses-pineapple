5878c7a90064a6c3cd8f904f077aaabf0b00ded0
Shawn Seymour <musicalshawn@gmail.com>
2016-03-22 18:57:20 -0500

    Adding authentication to server side endpoints

--- server/api/upload/index.js
+++ server/api/upload/index.js
@@ -2,14 +2,15 @@
 
 var express = require('express');
 var controller = require('./upload.controller');
+import * as auth from '../../auth/auth.service';
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.hasRole('organizer'), controller.index);
+router.get('/:id', auth.hasRole('organizer'), controller.show);
+router.post('/', auth.hasRole('organizer'), controller.create);
+router.put('/:id', auth.hasRole('organizer'), controller.update);
+router.patch('/:id', auth.hasRole('organizer'), controller.update);
+router.delete('/:id', auth.hasRole('organizer'), controller.destroy);
 
 module.exports = router;
