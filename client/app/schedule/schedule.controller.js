de940d13d953b5a18b354e6142b12685354cb747
devshawn <musicalshawn@gmail.com>
2016-03-24 19:52:14 -0500

    Updating master schedule

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -5,6 +5,49 @@ angular.module('ulyssesApp')
     var self = this;
     self.data = {"times" : []};
     self.times = [800,815,830,845,900,915,930,945,1000,1015,1030,1045,1100,1115,1130,1145,1200,1215,1230,1245,1300,1315,1330,1345,1400,1415,1430,1445,1500,1515,1530,1545,1600,1615,1630,1645,1700,1715,1730,1745,1800];
+    //self.times = [800, 815, 830, 845, 1700];
+    self.width = 10.0;
+    //self.times2 = [{"time" : 800, jobs}]
+    self.jobs = Job.query({}, function(results) {
+      self.width = ((100 / results.length) - (17  / results.length)) + "%";
+      console.log(self.width);
+    });
+
+    self.getRow = function(time) {
+      console.log("Run once");
+      var result = {"slots" : []};
+      Job.query({}, function(results) {
+        var jobs = results;
+        jobs.forEach(function(job) {
+          Slot.query({jobID: job._id}, function(results2) {
+            var slots = results2;
+            slots.forEach(function(slot) {
+              if(slot.start <= time && time <= slot.end) {
+                //console.log("YES");
+                result["slots"].push({"id" : slot._id, "jobID" : job._id});
+              } else {
+                //console.log("NO");
+              }
+            });
+          });
+        });
+      });
+      return result;
+    }
+
+    self.displaySlot = function(jobID, data) {
+      if (data) {
+        var data2 = "";
+        data.forEach(function (element) {
+          if (element["jobID"] == jobID) {
+            data2 = element["id"];
+          }
+        });
+
+        return data2;
+      }
+    }
+
 
     self.parseTime = function(time) {
       if(time) {
@@ -77,6 +120,5 @@ angular.module('ulyssesApp')
       }
 
 
-      self.createSchedule();
     }
   });
